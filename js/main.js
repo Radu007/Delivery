ca45f62761b7f113f9334aa1fb776146effc764d
Radu <cazacuraduic@gmail.com>
2020-04-10 18:37:31 +0300

    Mai ai de lucru urod

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.querySelector('#cart-button').addEventListener('click', function (e) {
+	document.querySelector('.modal').classList.add('is-open')
+})
+
+document.querySelector('.close').addEventListener('click', function (e) {
+	document.querySelector('.modal').classList.remove('is-open')
+})
+
+
+new WOW().init();
